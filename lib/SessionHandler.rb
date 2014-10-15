93a23abb4b84b39aaace380ac0c0935855e5e191
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2014-10-15 10:30:37 +0200

    Added a different color to the track that is running now

--- lib/SessionHandler.rb
+++ lib/SessionHandler.rb
@@ -15,6 +15,10 @@ def get_track(id)
 	$tracksStorage[id.to_s] || {}
 end
 
+def is_running_now(time, duration)
+  is_track_running_now(Time.now, time, duration)
+end
+
 def get_tracks_for_now()
 	now = Time.now
 	tracks_now = {}
@@ -33,16 +37,16 @@ end
 
 def is_track_running_now(now, track_time, duration)
 	t_a = track_time.split(/:/)
-    end_hour = t_a[0].to_i
-    end_minutes = t_a[1].to_i + duration.to_i
-    while end_minutes >= 60 do
-    	end_hour += 1
-    	end_minutes -= 60
-    end
-    startTime = Time.mktime(now.year, now.month, now.day, t_a[0], t_a[1])
-    endTime = Time.mktime(now.year, now.month, now.day, end_hour, end_minutes)
-
-    startTime < now && endTime > now
+  end_hour = t_a[0].to_i
+  end_minutes = t_a[1].to_i + duration.to_i
+  while end_minutes >= 60 do
+  	end_hour += 1
+  	end_minutes -= 60
+  end
+  startTime = Time.mktime(now.year, now.month, now.day, t_a[0], t_a[1])
+  endTime = Time.mktime(now.year, now.month, now.day, end_hour, end_minutes)
+  
+  startTime < now && endTime > now
 end
 
 def get_spot_name(id)
