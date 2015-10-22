126fe24f292901bfa9aaa8e3f29b8e16022bea5c
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2015-10-22 21:54:39 +0200

    Upgraded to lasted DUT Need to find an easier way to do this...

--- routes/main.rb
+++ routes/main.rb
@@ -2,10 +2,13 @@ class DUT < Sinatra::Application
   get '/' do
     site_mode = Configuration.find_by_name("site_mode")
     if site_mode.value == "track"
-      @tracks = Track.order("title DESC")
-      haml :track_index
+      event_id = Configuration.find_by_name("event").value
+      @tracks = Talk.where('event_id = ?', event_id).select('track').distinct.map { | talk |
+        talk.track
+      }
+      haml :index_track
     else
-      haml :cfp_index
+      haml :index_cfp
     end
   end
 end
